556ff61d655010354c5e5e89459d44150001a6a1
nabesan184 <simeji.gm@gmail.com>
2019-07-14 13:49:08 +0900

    category選択機能追加したよ

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :category
+  
   validates :title, presence: true, length: {maximum: 50}
   validates :body,  presence: true, length: {maximum: 2000}
 end
