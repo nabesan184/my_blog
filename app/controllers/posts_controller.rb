8c38151902556c003557fe2252461903f2451d9c
nabesan184 <simeji.gm@gmail.com>
2019-07-14 11:59:40 +0900

    リファクタリング終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def  index
-    @posts = Post.all
-    @new_posts = Post.all
-    
+    @posts = Post.order(created_at: :desc)
+    @new_posts = Post.order(created_at: :desc).limit(5)
+
   end
 
   def new
@@ -19,14 +21,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
-  end 
+  end
+  
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: "ブログを更新しました。"
     else
@@ -35,7 +35,6 @@ class PostsController < ApplicationController
   end
 
 def destroy
-  @post = Post.find(params[:id])
   @post.destroy
   redirect_to posts_path, notice: "ブログを削除しました。"
 end
@@ -44,5 +43,9 @@ end
 
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
